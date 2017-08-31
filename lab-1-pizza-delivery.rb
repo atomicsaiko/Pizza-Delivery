1af8ba42731ff669f1eed26d5cea3e64cdc8e5ce
atomicsaiko <atomicsaiko@gmail.com>
2017-08-31 15:30:50 +0200

    add pizza to order

--- lab-1-pizza-delivery.rb
+++ lab-1-pizza-delivery.rb
@@ -1,9 +1,11 @@
 require_relative "menu.rb"
+require_relative "order.rb"
 
 done = false
 
 puts "Hello, welcome to our pizza express delivery"
 
+order = Order.new
 menu = Menu.new
 
 #this will run until done is set to true
@@ -11,12 +13,16 @@ while not done
   puts "What can I do for you?"
   puts "1: Order a pizza"
   puts "2: Nothing"
+  puts "3: Check out (€ #{order.value})" if order.value > 0
   puts "------------------------------"
 
   #get some input from the customer
   choice = gets.chomp.to_i
   case choice
     when 1
+      menu.print
+      pizza = menu.make_choice
+      order.add(pizza)
       puts "Let me show you the menu"
     when 2
       done = true
