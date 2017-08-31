f332d79ee771d251fbb1f774c6314651eb3611ce
atomicsaiko <atomicsaiko@gmail.com>
2017-08-31 15:35:04 +0200

    add pizza to order

--- lab-1-pizza-delivery.rb
+++ lab-1-pizza-delivery.rb
@@ -26,6 +26,13 @@ while not done
       puts "Let me show you the menu"
     when 2
       done = true
+    when 3
+      order.print
+      puts "Do you wish to pay with"
+      puts "1: ideal"
+      puts "2: creditcard"
+      payment = gets.chomp.to_i
+      done = (payment == 1 || payment == 2)
     else
       puts "I dont understand"
   end
